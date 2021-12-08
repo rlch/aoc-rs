ac744196700c69aad106fea49f8b0a9d8cc0e4b7
RJM <rjmbowie@gmail.com>
2021-12-08 12:23:28 +1100

    feat: add day1 part2; compile binary

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,5 @@
 use clap::{App, Arg};
-use std::{fs, process::exit};
+use std::{fs, process::exit, usize};
 
 fn main() {
     let matches = App::new("Day 1: Sonar Sweep")
@@ -13,23 +13,52 @@ fn main() {
                 .index(1)
                 .required(true),
         )
+        .arg(
+            Arg::new("batch")
+                .short('b')
+                .long("batch")
+                .takes_value(true)
+                .value_name("INT")
+                .default_value("1"),
+        )
         .get_matches();
 
-    if let Some(filename) = matches.value_of("file") {
-        let input = fs::read_to_string(filename)
-            .unwrap_or_else(|_| panic!("Could not read your input at `{}`.", filename));
+    let batch_size: usize = matches
+        .value_of("batch")
+        .unwrap()
+        .parse()
+        .expect("Expected batch size to be a positive integer.");
 
-        let mut prev_depth: u32 = u32::MAX;
-        let mut increases: u32 = 0;
-        for line in input.lines() {
-            let depth = line.parse::<u32>().unwrap();
-            if depth > prev_depth {
-                increases += 1;
+    let input =
+        fs::read_to_string(matches.value_of("file").unwrap()).expect("Could not read your input.");
+
+    let mut batch_sums = Vec::<u32>::new();
+
+    let mut curr_sums = vec![0; batch_size];
+    for (n, line) in input.lines().enumerate() {
+        let depth = line.parse::<u32>().unwrap();
+        for i in 0..batch_size {
+            if n.checked_sub(i) == None {
+                continue;
             }
-            prev_depth = depth;
+            curr_sums[(n - i) % 3] += depth;
         }
 
-        println!("The depth increased {} times.", increases);
-        exit(0);
+        if n + 1 >= batch_size {
+            batch_sums.push(curr_sums[(n + 1) % 3]);
+            curr_sums[(n + 1) % 3] = 0;
+        }
+        // println!("n: {}, curr: {:?}, batch: {:?}", n, curr_sums, batch_sums);
     }
+
+    // println!("{:?}", batch_sums);
+
+    let mut prev = u32::MAX;
+    let increases = batch_sums.iter().fold(0, |i, sum| {
+        let increased = sum > &prev;
+        prev = *sum;
+        i + if increased { 1 } else { 0 }
+    });
+    println!("The depth increased {} times.", increases);
+    exit(0);
 }
