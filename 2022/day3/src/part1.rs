47194fe4fb17cc81623703b2488f8fb0c1479db7
RJM <rjmbowie@gmail.com>
2022-12-24 22:49:33 +1100

    Finish day 3

--- 2022/day3/src/part1.rs
+++ 2022/day3/src/part1.rs
@@ -1,3 +1,35 @@
-pub fn run(input: String) -> impl std::fmt::Display {
+use std::collections::HashSet;
+
+pub fn run(input: String) -> u32 {
     input
+        .lines()
+        .map::<char, _>(|l| {
+            let n = l.len();
+            let first = l.chars().take(n / 2);
+            let second = l.chars().skip(n / 2);
+            return *HashSet::<char>::from_iter(first)
+                .intersection(&HashSet::from_iter(second))
+                .next()
+                .expect("no intersection found");
+        })
+        .map(|common| match common {
+            'a'..='z' => 1 + common as u32 - 97,
+            'A'..='Z' => 27 + common as u32 - 65,
+            _ => unreachable!(),
+        })
+        .sum::<u32>()
+}
+
+#[test]
+fn example() {
+    let input = r#"
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"#
+    .trim();
+    assert_eq!(run(input.to_string()), 157);
 }
--- 2022/day3/src/part2.rs
+++ 2022/day3/src/part2.rs
@@ -1,3 +1,35 @@
-pub fn run(input: String) -> impl std::fmt::Display {
-    input
+use std::collections::HashSet;
+
+pub fn run(input: String) -> u32 {
+    let mut sum = 0;
+    let lines = input.lines().collect::<Vec<&str>>();
+    for i in (0..input.lines().count()).step_by(3) {
+        let a = HashSet::<char>::from_iter(lines[i].chars());
+        let b = HashSet::<char>::from_iter(lines[i + 1].chars());
+        let c = HashSet::<char>::from_iter(lines[i + 2].chars());
+        let common = *HashSet::from_iter(a.intersection(&b).copied())
+            .intersection(&c)
+            .next()
+            .expect("no intersection found");
+        sum += match common {
+            'a'..='z' => 1 + common as u32 - 97,
+            'A'..='Z' => 27 + common as u32 - 65,
+            _ => unreachable!(),
+        }
+    }
+    sum
+}
+
+#[test]
+fn example() {
+    let input = r#"
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"#
+    .trim();
+    assert_eq!(run(input.to_string()), 70);
 }
